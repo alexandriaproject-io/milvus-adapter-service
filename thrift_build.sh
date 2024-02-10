3abd96f3ea01d02a79c7304c57b840909e910917
NiftyliuS <niftylius@gmail.com>
2024-02-10 04:23:19 +0200

    better thrift build

--- thrift_build.sh
+++ thrift_build.sh
@@ -1,3 +1,24 @@
-thrift --gen py -out .\ .\thrift\com.milvus.nats.thrift
+#!/bin/bash
 
-thrift --gen html -out .\src\services\status_server\static .\thrift\com.milvus.nats.thrift
+# Check the first argument for the language flag and use the second argument as the output directory if provided
+if [[ "$1" == "--py" ]]; then
+    py_outdir="${2:-./}" # Corrected syntax for default value
+    mkdir -p "$py_outdir"
+    thrift --gen py -out $py_outdir ./thrift/com.milvus.nats.thrift
+
+elif [[ "$1" == "--html" ]]; then
+    html_outdir="${2:-./src/services/status_server/static}" # Corrected syntax for default value
+    mkdir -p "$html_outdir"
+    thrift --gen html -out $html_outdir ./thrift/com.milvus.nats.thrift
+
+elif [[ "$1" == "--golang" ]]; then
+    golang_outdir="${2:-./}" # Corrected syntax for default value
+    mkdir -p "$golang_outdir"
+    thrift --gen go -out $golang_outdir ./thrift/com.milvus.nats.thrift
+
+elif [[ "$1" == "--ts" ]]; then
+    ts_outdir="${2:-./}" # Assuming default TypeScript output directory
+    mkdir -p "$ts_outdir"
+    # Assuming "ts" as the generator name for TypeScript, adjust as needed
+    thrift --gen js:ts -out $ts_outdir ./thrift/com.milvus.nats.thrift
+fi
